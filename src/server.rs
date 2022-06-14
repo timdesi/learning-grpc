f84ca8525b986f68d5f3ff1961e6de2e4868be38
timdesi <desislav.iliev@bosch.io>
2022-06-14 10:38:35 +0300

    test1

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,8 @@
 use futures::Stream;
-use tokio::sync::mpsc;
-use tokio_stream::StreamExt;
-use tokio_stream::wrappers::ReceiverStream;
 use std::pin::Pin;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::StreamExt;
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
@@ -10,13 +10,13 @@ use hello::hello_server::{Hello, HelloServer};
 use hello::{HelloRequest, HelloResponse};
 use learning_grpc::hello;
 
+use dotenv::dotenv;
 use downloader::downloader_server::{Downloader, DownloaderServer};
 use downloader::{
     CancelRequest, DataChunk, DownloadRequest, DownloadResponse, DownloadResult, GetRequest,
 };
 use learning_grpc::downloader;
 use log::{debug, error, info};
-use dotenv::dotenv;
 
 #[derive(Default)]
 pub struct DownloaderService {}
@@ -40,18 +40,24 @@ impl Downloader for DownloaderService {
         Pin<Box<dyn Stream<Item = Result<DownloadResponse, Status>> + Send + Sync + 'static>>;
     type GetStream = Pin<Box<dyn Stream<Item = Result<DataChunk, Status>> + Send + Sync + 'static>>;
 
-    async fn get(&self, request: Request<GetRequest>) -> Result<Response<Self::GetStream>, Status> {
-        
+    async fn get(
+        &self,
+        _request: Request<GetRequest>,
+    ) -> Result<Response<Self::GetStream>, Status> {
+        debug!("Got a request: {:?}", _request);
+
         let (tx, rx) = mpsc::channel(4);
 
-           //let planets: Vec<u8> = vec![0,0,0];
+        //let planets: Vec<u8> = vec![0,0,0];
 
-           tokio::spawn(async move {
+        tokio::spawn(async move {
             //let mut stream = tokio_stream::iter(&planets);
 
             tx.send(Ok(DataChunk {
-                    data: vec![0,1,2],
-                })).await.unwrap();
+                data: vec![0, 1, 2],
+            }))
+            .await
+            .unwrap();
 
             // while let Some(planet) = stream.next().await {
             //     tx.send(Ok(DataChunk {
@@ -62,27 +68,30 @@ impl Downloader for DownloaderService {
             // }
         });
 
-        Ok(Response::new(Box::pin(
-            ReceiverStream::new(rx),
-        )))
-
+        Ok(Response::new(Box::pin(ReceiverStream::new(rx))))
     }
 
     async fn download(
         &self,
-        _: Request<DownloadRequest>,
+        _request: Request<DownloadRequest>,
     ) -> Result<Response<Self::DownloadStream>, Status> {
-                
+        debug!("Got a request: {:?}", _request);
+
+        let url= _request.into_inner().software_artifact.unwrap().url;
+        debug!("Got a request url: {:?}", url);
+
         let (tx, rx) = mpsc::channel(4);
 
-           //let planets: Vec<u8> = vec![0,0,0];
+        //let planets: Vec<u8> = vec![0,0,0];
 
-           tokio::spawn(async move {
+        tokio::spawn(async move {
             //let mut stream = tokio_stream::iter(&planets);
 
             tx.send(Ok(DownloadResponse {
-                    ..Default::default()
-                })).await.unwrap();
+                ..Default::default()
+            }))
+            .await
+            .unwrap();
 
             // while let Some(planet) = stream.next().await {
             //     tx.send(Ok(DataChunk {
@@ -93,16 +102,17 @@ impl Downloader for DownloaderService {
             // }
         });
 
-        Ok(Response::new(Box::pin(
-            ReceiverStream::new(rx),
-        )))
-
+        Ok(Response::new(Box::pin(ReceiverStream::new(rx))))
     }
 
     async fn cancel_download(
         &self,
-        _: Request<CancelRequest>,
+        _request: Request<CancelRequest>,
     ) -> Result<Response<DownloadResult>, Status> {
+        debug!("Got a request: {:?}", _request);
+
+        let da= _request.into_inner().operation_id;
+
         let response = DownloadResult {
             code: 0,
             downloaded_artifact: "downloaded artifact".to_string(),
@@ -114,7 +124,6 @@ impl Downloader for DownloaderService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     dotenv().ok();
     env_logger::init();
 
